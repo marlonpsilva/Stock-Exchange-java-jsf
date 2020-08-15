8988974f61a966e3228a6a4bc01c3fc319d1adcd
marlonpsilva <marlon_ps21@hotmail.com>
2020-08-15 11:55:58 -0300

    Configurando o pprojeto: gitignore criando primeiro modelo - Negociacao.java

---
+++ src/main/java/br/com/verum/modelo/Negociacao.java
@@ -0,0 +1,28 @@
+package br.com.verum.modelo;
+
+import java.time.LocalDateTime;
+
+public final class Negociacao {
+
+	private final double preco;
+	private final int quantidade; // Quantidade de ações
+	private final LocalDateTime data;
+
+	public Negociacao(double preco, int quantidade, LocalDateTime data) {
+		this.preco = preco;
+		this.quantidade = quantidade;
+		this.data = data;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+}

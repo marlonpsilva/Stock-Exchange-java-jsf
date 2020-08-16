a4c9180fc1f43a451ccfb10ce153cc0543c500af
marlonpsilva <marlon_ps21@hotmail.com>
2020-08-16 14:16:40 -0300

    Implementação do client que retorna a lista de negociações do ws

---
+++ src/main/java/br/com/verum/ws/ClientWebservice.java
@@ -0,0 +1,45 @@
+package br.com.verum.ws;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import br.com.verum.modelo.Negociacao;
+import br.com.verum.reader.LeitorXml;
+
+public class ClientWebservice {
+
+	public static final String URL_WEBSERVICES = "https://argentumws-spring.herokuapp.com/negociacoes";
+
+	public List<Negociacao> getNegociacoes() {
+
+		HttpURLConnection connection = null;
+
+		try {
+
+			URL url = new URL(URL_WEBSERVICES);
+			connection = (HttpURLConnection) url.openConnection();
+			InputStream content = connection.getInputStream();
+			return new LeitorXml().carrega(content);
+
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			connection.disconnect();
+		}
+
+	}
+	
+	public static void main(String[] args) {
+		
+		ClientWebservice ws = new ClientWebservice();
+		
+		List<Negociacao> negociacoes = ws.getNegociacoes();
+		
+		for (Negociacao negociacao : negociacoes) {
+			System.out.println(negociacao.getPreco());
+		}
+	}
+}
